b7fb40ae20e88f8ec85d16a6b9f207a21163ec2b
Alex Fowler <noncom@130043.simplecloud.club>
2017-07-12 12:38:25 +0300

    + initial commit

---
+++ core/src/main/java/com/example/noncom/jme_android_example_as3_canary6/Game.java
@@ -0,0 +1,14 @@
+package com.example.noncom.jme_android_example_as3_canary6;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ * Created by noncom on 7/12/17.
+ */
+
+public class Game extends SimpleApplication {
+    @Override
+    public void simpleInitApp() {
+
+    }
+}
---
+++ desktop/src/main/java/com/example/noncom/jme_android_example_as3_canary6/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.example.noncom.jme_android_example_as3_canary6;
+/**
+ * Created by noncom on 7/12/17.
+ */
+
+public class DesktopLauncher {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
